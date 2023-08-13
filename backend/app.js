b091ccce37aa228afb10789c8e21d94fd21661d8
sscherbett <sch3rbett@yandex.ru>
2023-08-14 00:36:40 +0300

    Initial commit

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const routes = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 4000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 app.use(cors({ origin: ['http://localhost:3001', 'http://scherbett.frontend.nomoreparties.co', 'https://scherbett.frontend.nomoreparties.co'] }));
 
